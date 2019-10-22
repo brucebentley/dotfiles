09a5968c74a2d1d2ecd0c1ba45512aa378d27431
Bruce Bentley <brucebentley@users.noreply.github.com>
2019-10-22 09:49:34 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+
+#!/bin/sh
+
+if [ ! -d "$HOME/.yadr" ]; then
+    echo "Installing YADR for the first time"
+    git clone --depth=1 https://github.com/brucebentley/dotfiles.git "$HOME/.yadr"
+    cd "$HOME/.yadr"
+    [ "$1" = "ask" ] && export ASK="true"
+    rake install
+else
+    echo "YADR is already installed"
+fi
