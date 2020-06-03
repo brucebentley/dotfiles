e30dbff8f913254752e5995ecec9c491bce4a7bb
Bruce Bentley <brucebentley@me.com>
2020-06-03 04:27:39 -0400

    Easier automations for installing

--- brew.sh
+++ brew.sh
@@ -1,147 +1,266 @@
 #!/usr/bin/env bash
 
-# MAKE SURE WE’RE USING THE LATEST HOMEBREW.
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+# Install Command-Line Tools Using Homebrew
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# Make Sure We’re Using The Latest Homebrew
 brew update
 
-# UPGRADE ANY ALREADY-INSTALLED FORMULAE.
+# Upgrade Any Already-Installed Formulae
 brew upgrade
 
-# SAVE HOMEBREW'S INSTALLED LOCATION.
+# Save Homebrew’s Installed Location
 BREW_PREFIX=$(brew --prefix)
 
 
-##################################################
-# INSTALL ESSENTIALS                             #
-##################################################
-# INSTALL GNU CORE UTILITIES (THOSE THAT COME WITH MACOS ARE OUTDATED).
-# DON’T FORGET TO ADD THE FOLLOWING TO YOUR $PATH.
-#     $(brew --prefix coreutils)/libexec/gnubin
-brew install coreutils
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Install Core Utilities
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+
+# Install GNU Core Utilities
+#   Don't Forget To Add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`
+brew install coreutils              # GNU File, Shell, & Text utilities
 ln -s "${BREW_PREFIX}/bin/gsha256sum" "${BREW_PREFIX}/bin/sha256sum"
 
-# INSTALL SOME OTHER USEFUL UTILITIES LIKE `sponge`.
-brew install moreutils
+core_tools_list=(
+    moreutils                       # Collection of tools that nobody wrote when UNIX was young
+    findutils                       # Collection of GNU `find`, `xargs`, & `locate`
+    gnu-sed --with-default-names    # GNU implementation of the famous stream editor
+    bash                            # Bourne-Again SHell, a UNIX command interpreter
+    bash-completion2                # Programmable completion for Bash 4.1+
+    zsh                             # UNIX shell ( command interpreter )
+    zsh-completions                 # Additional completion definitions for zsh
+    gnupg                           # GNU Pretty Good Privacy ( PGP ) package
+)
+for core_tool in "${core_tools_list[@]}"
+do
+    brew install "${core_tool}"
+done
 
-# INSTALL GNU `find`, `locate`, `updatedb`, AND `xargs`, `g`-prefixed.
-brew install findutils
-
-# Install GNU `sed`, OVERWRITING THE BUILT-IN `sed`.
-brew install gnu-sed
-
-# INSTALL A MODERN VERSION OF BASH.
-# NOTE: DON’T FORGET TO ADD `/usr/local/bin/bash` TO `/etc/shells` BEFORE
-# RUNNING `chsh`.
-brew install bash
-brew install bash-completion2
-
-# SWITCH TO USING BREW-INSTALLED BASH AS DEFAULT SHELL
-if ! grep -F -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
-    echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
-    chsh -s "${BREW_PREFIX}/bin/bash";
+# Switch to using brew-installed bash as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/bash" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/bash" | sudo tee -a /etc/shells;
+  #chsh -s "${BREW_PREFIX}/bin/bash";
 fi;
 
-# INSTALL `wget` WITH IRI SUPPORT.
-brew install wget
-
-# INSTALL GnuPG TO ENABLE PGP-SIGNING COMMITS.
-brew install gnupg
-
-# INSTALL MORE RECENT VERSIONS OF SOME MACOS TOOLS.
-brew install vim
-brew link vim
-brew install grep
-brew install openssh
-brew install screen
-#brew install php
-brew install gmp
+# Switch to using brew-installed zsh as default shell
+if ! fgrep -q "${BREW_PREFIX}/bin/zsh" /etc/shells; then
+  echo "${BREW_PREFIX}/bin/zsh" | sudo tee -a /etc/shells;
+  chsh -s "${BREW_PREFIX}/bin/zsh";
+fi;
 
 
-##################################################
-# INSTALL FONT TOOLS                             #
-##################################################
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Install More Newer Versions Of macOS Tools
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+macos_tools_list=(
+    vim                         # Vi 'workalike' with many additional features
+    wget                        # Internet file retriever
+    curl                        # Get a file from an HTTP, HTTPS or FTP server
+    grep                        # GNU `grep`, `egrep` & `fgrep`
+    make                        # Utility for directing compilation
+    openssh                     # OpenBSD freely-licensed SSH connectivity tools
+    openssl                     # Cryptography & SSL/TLS Toolkit
+    screen                      # Terminal multiplexer with VT100/ANSI terminal emulation
+    #php                        # General-purpose scripting language
+    gmp                         # GNU multiple precision arithmetic library
+)
+for macos_tool in "${macos_tools_list[@]}"
+do
+    brew install "${macos_tool}"
+done
+
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Install Git & Some Useful Addons
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+git_tools_list=(
+    git                         # Distributed revision control system
+    git-lfs                     # Git extension for versioning large files
+    git-extras                  # Small git utilities
+    git-flow                    # Extensions to follow Vincent Driessen's branching model
+    git-secret                  # Bash-tool to store the private data inside a git repo
+)
+for git_tool in "${git_tools_list[@]}"
+do
+    brew install "${git_tool}"
+done
+
+
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Install Font Tools
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
 #brew tap bramstein/webfonttools
-#brew install sfnt2woff
-#brew install sfnt2woff-zopfli
-#brew install woff2
+font_tools_list=(
+    #sfnt2woff                  # Convert existing TrueType/OpenType fonts to WOFF format
+    #sfnt2woff-zopfli           # WOFF utilities with Zopfli compression
+    #sfntly                     # Library for Using, Editing, and Creating SFNT-based Fonts
+    #woff2                      # Compress fonts with Brotli into WOFF2 format
+    #freetype                   # Software library to render fonts
+)
+for font_tool in "${font_tools_list[@]}"
+do
+    brew install "${font_tool}"
+done
 
-# LIBRARY TO RENDER FONTS.
-brew install freetype
-
-
-##################################################
-# INSTALL SOME CTF TOOLS                         #
-#     SEE https://github.com/ctfs/write-ups      #
-##################################################
-#brew install aircrack-ng
-#brew install bfg
-#brew install binutils
-#brew install binwalk
-#brew install cifer
-#brew install dex2jar
-#brew install dns2tcp
-#brew install fcrackzip
-#brew install foremost
-#brew install hashpump
-#brew install hydra
-#brew install john
-#brew install knock
-#brew install netpbm
-#brew install nmap
-#brew install pngcheck
-#brew install socat
-#brew install sqlmap
-#brew install tcpflow
-#brew install tcpreplay
-#brew install tcptrace
-#brew install ucspi-tcp # `tcpserver` etc.
-#brew install xpdf
-brew install xz
-
-
-##################################################
-# INSTALL OTHER USEFUL BINARIES                  #
-##################################################
-brew install ack
-# PERL LIBRARY FOR READING, WRITING & EDITING EXIF METADATA.
-brew install exiftool
-#brew install exiv2
-brew install git
-brew install git-lfs
-# BASH-TOOL TO STORE THE PRIVATE DATA INSIDE A GIT REPO.
-brew install git-secret
-brew install gs
-# TOOLS AND LIBRARIES TO MANIPULATE IMAGES IN MANY FORMATS.
-brew install imagemagick --with-webp
-brew install lua
-#brew install lynx
-#brew install p7zip
-#brew install pigz
-brew install pv
-brew install rename
-#brew install rlwrap
-brew install ssh-copy-id
-brew install tree
-#brew install vbindiff
-brew install yarn --ignore-dependencies
-#brew install zopfli
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Install Some CTF Tools
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# See: https://github.com/ctfs/write-ups.
+#
+ctf_tools_list=(
+    #aircrack-ng                # Next-generation aircrack with lots of new features
+    #bfg                        # Remove large files or passwords from Git history like git-filter-branch
+    #binutils                   # GNU binary tools for native development
+    #binwalk                    # Searches a binary image for embedded files and executable code
+    #cifer                      # Work on automating classical cipher cracking in C
+    #dex2jar                    # Tools to work with Android .dex and Java .class files
+    #dns2tcp                    # TCP over DNS tunnel
+    #fcrackzip                  # Zip password cracker
+    #foremost                   # Console program to recover files based on their headers and footers
+    #hashpump                   # Tool to exploit hash length extension attack
+    #hydra                      # Network logon cracker which supports many services
+    #john                       # Featureful UNIX password cracker
+    #knock                      # Port-knock server
+    #netpbm                     # Image manipulation
+    #nmap                       # Port scanning utility for large networks
+    #pngcheck                   # Print info and check PNG, JNG, and MNG files
+    #socat                      # SOcket CAT: netcat on steroids
+    #sqlmap                     # Penetration testing for SQL injection and database servers
+    #tcpflow                    # TCP flow recorder
+    #tcpreplay                  # Replay saved tcpdump files at arbitrary speeds
+    #tcptrace                   # Analyze tcpdump output
+    #ucspi-tcp                  # Tools for building TCP client-server applications
+    #xpdf                       # PDF Viewer
+    xz                          # General-purpose data compression with high compression ratio
+)
+for ctf_tool in "${ctf_tools_list[@]}"
+do
+    brew install "${ctf_tool}"
+done
 
 
-##################################################
-# DEVELOPMENT UTILITIES                          #
-##################################################
-# ENABLE SSL CERTIFICATE SUPPORT ON YOUR LOCAL DEV ENVIRONMENT.
-brew install mkcert
-# COMMAND-LINE COLLECTION RUNNER FOR POSTMAN.
-brew install newman
-brew install nss # IF YOU USE FIREFOX
-# AUTOFORMAT SHELL SCRIPT SOURCE CODE.
-brew install shfmt
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Install Other Useful Binaries
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+useful_tools_list=(
+    ack                         # Search tool like `grep`, but optimized for programmers
+    cmake                       # Cross-platform `make`
+    dnsmasq                     # Lightweight DNS forwarder & DHCP server
+    exiftool                    # Perl lib for reading & writing EXIF metadata
+    #exiv2                      # EXIF and IPTC metadata manipulation library & tools
+    gettext                     # GNU internationalization ( i18n ) & localization ( l10n ) library
+    go                          # Open source programming language to build simple/reliable/efficient software
+    gs                          # Interpreter for PostScript and PDF
+    #heroku                     #
+    htop                        # Improved top ( interactive process viewer )
+    imagemagick --with-webp     # Tools & libraries to manipulate images in many formats
+    jq                          # Lightweight and flexible command-line JSON processor
+    lua                         # Powerful, lightweight programming language
+    #lynx                       # Text-based web browser
+    mas                         # Mac App Store command-line interface
+    neovim                      # Ambitious Vim-fork focused on extensibility and agility
+    newman                      # Command-line collection runner for Postman
+    ngrep                       # Network Grep
+    nss                         # Libraries for security-enabled client and server applications
+    p7zip                       # 7-Zip ( high compression file archiver ) implementation
+    pigz                        # Parallel gzip
+    pinentry                    # Passphrase entry dialog utilizing the Assuan protocol
+    pinentry-mac                # Pinentry for GPG on Mac
+    pkg-con                     #
+    #pv                         # Monitor data's progress through a pipe
+    readline                    # Library for command-line editing
+    rename                      # Perl-powered file rename script with many helpful built-ins
+    rlwrap                      # Readline wrapper: adds readline support to tools that lack it
+    shellcheck                  # Static analysis and lint tool, for (ba)sh scripts
+    shfmt                       # Autoformat shell script source code
+    ssh-copy-id                 # Add a public key to a remote machine's authorized_keys file
+    thefuck                     # Programatically correct mistyped console commands
+    tmux                        # Terminal multiplexer
+    tree                        # Display directories as trees ( with optional color/HTML output )
+    #vbindiff                   # Visual Binary Diff
+    #zopfli                     # New `zlib` ( `gzip`, `deflate` ) compatible compressor
+)
+for useful_tool in "${useful_tools_list[@]}"
+do
+    brew install "${useful_tool}"
+done
 
 
-##################################################
-# CLEANUP                                        #
-##################################################
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Install Image & Video Editing Tools
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+media_tools_list=(
+    ffmpeg                      # Play, Record, Convert, & Stream Audio & Video
+    webp                        # Image Format Providing Lossless & Lossy Compression For Web Images
+    youtube-dl                  # Download Youtube Videos From The Command-Line
+)
+for media_tool in "${media_tools_list[@]}"
+do
+    brew install "${media_tool}"
+done
 
-# REMOVE OUTDATED VERSIONS FROM THE CELLAR.
+
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Install Development Tools
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+dev_tools_list=(
+    #helm                               # The Kubernetes Package Manager
+    imagemagick@6 --build-from-source   # Tools & Libraries To Manipulate Images In Many Formats
+    sphinx                              # Full-Text Search Engine
+    redis                               # Persistent Key-Value Database, With Built-In Net Interface
+    mysql@5.7                           # Open Source Relational Database Management System
+    overmind                            # Process Manager For Procfile-Based Applications And tmux
+    v8@3.15                             # Google's JavaScript Engine
+)
+for dev_tool in "${dev_tools_list[@]}"
+do
+    brew install "${dev_tool}"
+done
+
+# Unlink the latest version ( v 8.0.x ) of MySQL that was just installed by Sphinx:
+brew unlink mysql
+
+# Start `redis` now & set `redis` to launch on system restart:
+brew services start redis
+
+brew update
+brew install mysql@5.7
+
+# You'll want to make sure that the version of MySQL doesn't get automatically updated when
+# running `brew upgrade` in the future, so let's use `brew pin` to lock it in:
+brew link --force mysql@5.7
+brew pin mysql@5.7
+
+# After a successful installation, you can start the server & ensure it auto-starts in the future with:
+brew services start mysql@5.7
+
+# Run the secure installation setup for MySQL.
+#   DO NOT set a password for the `root` user during this setup!
+/usr/local/bin/mysql_secure_installation
+
+
+#
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# Cleanup
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+#
+# Remove Outdated Versions From The Cellar
 brew cleanup
-
