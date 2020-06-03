220410f7fa2a3eb1bc8579fe356f3bbf22a5652a
Bruce Bentley <brucebentley@me.com>
2020-06-03 03:31:11 -0400

    Includeding Colorls & Composer

---
+++ install/composer-install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+EXPECTED_CHECKSUM="$(wget -q -O - https://composer.github.io/installer.sig)"
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+ACTUAL_CHECKSUM="$(php -r "echo hash_file('sha384', 'composer-setup.php');")"
+
+if [ "$EXPECTED_CHECKSUM" != "$ACTUAL_CHECKSUM" ]
+then
+    >&2 echo 'ERROR: Invalid installer checksum'
+    rm composer-setup.php
+    exit 1
+fi
+
+#php composer-setup.php --quiet
+php composer-setup.php
+RESULT=$?
+rm composer-setup.php
+
+# Make Composer Accessible Globally by putting it in a directory exposed to your $PATH.
+mv composer.phar /usr/local/bin/composer
+echo "Composer moved to /usr/local/bin/ so it's accessible globally."
+exit $RESULT
